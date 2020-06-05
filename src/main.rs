40362cabad46fc2f1e9f56a220362bed19dff255
Dan White <github@bluetufa.com>
2020-06-04 19:57:20 -0600

    fixins

--- src/main.rs
+++ src/main.rs
@@ -4,53 +4,43 @@ extern crate rand;
 use ansi_term::Colour;
 use rand::Rng;
 use std::env::args;
-use std::fs;
-use std::io::stderr;
-use std::io::Write;
+use std::fs::File;
+use std::io::{BufRead, BufReader, stderr, Write};
 
 fn main() {
     let args: Vec<String> = args().skip(1).collect();
+    let entropy = rand::thread_rng().gen_range(0, 255);
 
-    let entropy = rand::thread_rng().gen();
-    match args.len() {
-        1 => {
-            match fs::read_to_string(String::from(&args[0])) {
-                Ok(f) => {
-                    let my_chars: Vec<_> = f.chars().collect();
-                    cat(my_chars, entropy);
-                }
-                Err(e) => {
-                    writeln!(stderr(), "Invalid input. {}", e).unwrap();
-                    std::process::exit(1);
-                }
-            };
-        }
-        _ => match read_stdin(entropy) {
-            _ => {}
+    match read(args, entropy) {
+        Err(e) => {
+            writeln!(stderr(), "Invalid input. {}", e).unwrap();
+                std::process::exit(1);
         },
+        _ => {}
+    }
+}
+
+fn read(args: Vec<String>, entropy: i32) -> std::io::Result<()> {
+    match args.len() {
+        1 => read_file(&String::from(&args[0]), entropy),
+        _ => read_stdin(entropy)
     }
 }
 
 fn rainbow(seed: f64) -> ansi_term::Colour {
-    // TODO: freq needs to be variable based on input content size.
-    // approaching 0.02 for larger content to 1.2 for multiple pages.
     let freq = 1.13;
     let factor = seed / 16.0;
 
-    let red_work = freq * factor as f64;
-    let red = red_work.sin().mul_add(127.0, 128.0) as u8;
-
-    let green_work = freq * factor + 2.0 * std::f64::consts::PI / 3.0 as f64;
-    let green = green_work.sin().mul_add(127.0, 128.0) as u8;
-
-    let blue_work = freq * factor + 4.0 * std::f64::consts::PI / 3.0 as f64;
-    let blue = blue_work.sin().mul_add(127.0, 128.0) as u8;
+    let red = (freq * factor as f64).sin().mul_add(127.0, 128.0) as u8;
+    let green = (freq * factor + 2.0 * std::f64::consts::PI / 3.0 as f64).sin().mul_add(127.0, 128.0) as u8;
+    let blue = ( freq * factor + 4.0 * std::f64::consts::PI / 3.0 as f64).sin().mul_add(127.0, 128.0) as u8;
 
     Colour::RGB(red, green, blue)
 }
 
 fn cat(chars: Vec<char>, entropy: i32) {
     let mut index = 1;
+
     for c in chars {
         let seed = f64::from(index + entropy);
         let colour = rainbow(seed);
@@ -58,6 +48,18 @@ fn cat(chars: Vec<char>, entropy: i32) {
         print!("{}", rainbow_char);
         index += 1;
     }
+}
+
+fn read_file(file_name: &str, entropy: i32) -> std::io::Result<()> {
+    let mut index = entropy;
+    let f = File::open(file_name)?;
+    for line in BufReader::new(f).lines() {
+        let mut stuff = line.unwrap();
+        stuff.push('\n');
+        cat(stuff.chars().collect(), index);
+        index += 1;
+    }
+    Ok(())
 }
 
 fn read_stdin(entropy: i32) -> std::io::Result<()> {
